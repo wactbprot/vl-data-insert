a0a4fa0441738b3d0dd58affd31201ea58f8a6eb
Wact B. Prot <wactbprot@gmail.com>
2020-11-07 15:24:38 +0100

    0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.wactbprot/vl-data-insert "0.1.0"
+(defproject org.clojars.wactbprot/vl-data-insert "0.1.1"
   :description "Functions to insert vaclab style data in vaclab style database documents."
   :url "https://github.com/wactbprot/vl-data-insert"
   :dependencies [[org.clojure/clojure "1.10.0"]]
